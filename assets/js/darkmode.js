eacbf764ddac911590597b33e015c6500444a6b8
Lee Yuna <88178231+lyn0310@users.noreply.github.com>
2025-12-06 03:05:18 +0000

    codespace commit

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,15 +1,21 @@
-// 저장된 값 확인
-let darkMode = localStorage.getItem("dark-mode");
+document.addEventListener("DOMContentLoaded", function () {
 
-if (darkMode === "enabled") {
-    document.body.classList.add("dark");
-}
+    let darkMode = localStorage.getItem("dark-mode");
 
-document.getElementById("dark-mode-toggle").onclick = function() {
-    document.body.classList.toggle("dark");
-    if (document.body.classList.contains("dark")) {
-        localStorage.setItem("dark-mode", "enabled");
-    } else {
-        localStorage.setItem("dark-mode", "disabled");
+    if (darkMode === "enabled") {
+        document.body.classList.add("dark");
     }
-};
+
+    const btn = document.getElementById("dark-mode-toggle");
+    if (!btn) return;
+
+    btn.onclick = function () {
+        document.body.classList.toggle("dark");
+
+        if (document.body.classList.contains("dark")) {
+            localStorage.setItem("dark-mode", "enabled");
+        } else {
+            localStorage.setItem("dark-mode", "disabled");
+        }
+    };
+});
